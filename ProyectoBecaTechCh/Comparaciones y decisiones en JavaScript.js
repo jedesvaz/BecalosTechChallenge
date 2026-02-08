dbf270c73c6ffbf450b9ea7f1e5eb1bb804cf095
jedesvaz <jedesvaz@gmail.com>
2026-02-08 12:27:00 -0700

    Add random grade evaluation logic with console output

---
+++ ProyectoBecaTechCh/Comparaciones y decisiones en JavaScript.js
@@ -0,0 +1,12 @@
+let nota = Math.random()*100;
+if (nota >= 90){
+    console.log("La nota es de", nota, "Excelente")
+} else if (nota >= 75 && nota <= 89){
+    console.log("La nota es de ", nota, "Bien")
+} else if (nota >= 60 && nota <= 74 ){
+    console.log("La nota es de ", nota, "Suficiente")
+} else {
+    console.log("La nota es de ", nota, "No aprueba")
+}
+     
+    
