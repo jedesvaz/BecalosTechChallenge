5377067e18aed97168a9ea1ac70501f6a367373a
jedesvaz <jedesvaz@gmail.com>
2026-07-13 13:40:45 -0600

    Add initial implementation of planet exploration module with data and display logic as a Node.js project

---
+++ moduloDesarrolloAvanzadoenJS/introaNodeJSynpm/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,31 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Krypton",
+    descripcion: "Planeta ficticio del universo de Superman.",
+    descubiertoEn: "1938"
+  },
+  {
+    nombre: "Vilgaxia",
+    descripcion: "Un planeta ficticio del universo de Ben 10, hogar del villano Vilgax.",
+    descubiertoEn: "2005"
+  },
+  {
+    nombre: "Trántor",
+    descripcion: "Un planeta ficticio del universo de Fundación de Isaac Asimov, conocido por ser el centro del Imperio Galáctico.",
+    descubiertoEn: "1951"
+  }
+
+  // ¡Añade más planetas!
+];
+
+module.exports = planetas;
